7301fbf5f33136c2dff9c03df4ec3416b42b95a1
Sathish Gurka <157489950+sathishyadav024@users.noreply.github.com>
2025-05-07 19:42:16 +0530

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtPage />
+</template>
+<script setup>
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/assets/imgs/logo/favicon/favicon.ico",
+    },
+  ],
+});
+</script>
