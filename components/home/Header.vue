ec139b295b43d5482c6bcddf3f5b393411a57624
Gurka Sathish <sathishgurka@gmail.com>
2025-05-12 17:31:17 +0530

    checking the webhook

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -10,7 +10,7 @@
           <div class="text mb-100">
             <h4>
 We prioritizes in providing high-quality design and branding
-              solutions to businesses and individuals.
+              solutions to businesses and individuals.R
             </h4>
           </div>
         </div>
