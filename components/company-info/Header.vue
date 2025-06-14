e5a81c04643ae6275900f4cebaea54c1c61b0d81
Gurka Sathish <sathishgurka@gmail.com>
2025-06-14 16:04:01 +0530

    Modified content for company info page

--- components/company-info/Header.vue
+++ components/company-info/Header.vue
@@ -5,7 +5,7 @@
         <div class="col-lg-10">
           <div class="caption">
             <h2>
-              For over a decade, Revolynk Techs has been turning ideas into reality through cutting-edge web and app development, scalable cloud solutions, and impactful digital marketing. We’re always looking for passionate professionals to help shape the digital future.
+              For more than 10 years, we’ve been building web, cloud, and mobile solutions that drive digital transformation. Revolynk Techs is always focused on delivering with passion and precision.
             </h2>
             <div class="mt-30">
               <a href="/dark/home-main">Home</a>
