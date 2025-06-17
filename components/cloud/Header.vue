0f43541842b3da854acc866c41ab31b6fe54ad95
Gurka Sathish <sathishgurka@gmail.com>
2025-06-17 12:00:55 +0530

    updated content for cloud  page

--- components/cloud/Header.vue
+++ components/cloud/Header.vue
@@ -16,7 +16,7 @@
               </a>
             </div>
             <h1 class="fz-55 mt-30">
-              Network of wormholes colonies extraordinary claims require.
+              Scale smarter with secure, flexible cloud solutions
             </h1>
           </div>
         </div>
